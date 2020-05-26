9e8bbcaebbdc5c728e4a83449e20b078cd86905a
vagrant <vagrant@localhost.localdomain>
2020-05-26 09:02:16 +0000

    [Start]5-3

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,22 @@
+class PostImagesController < ApplicationController
+	def new
+		@post_images = PostImage.all
+    end
+    # 投稿データの保存
+    def create
+    	@post_image = PostImage.new(post_image_params)
+        @post_image.user_id = current_user.id
+        @post_image.save
+        redirect_to post_images_path
+    end
+    def index
+    end
+    def show
+        @post_image = PostImage.find(params[:id])
+    end
+
+    private
+    def post_image_params
+    	params.require(:post_image).permit(:shop_name,:image,:caption)
+    end
+end
